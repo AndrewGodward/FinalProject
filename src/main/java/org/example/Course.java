b7ed090499d4ac9d5ae2f6184b08e8bcf95f079c
%username% <%email%>
2024-05-25 18:17:18 -0400

    Added fields for Course and student class

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Course {
+    private String courseId; //C-departmentId-twoDigitCourseId
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+    private ArrayList<Double> finalScores;
+    private static int nextId;
 }
