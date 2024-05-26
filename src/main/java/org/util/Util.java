4639f8fd4887a9ca6d7d125b80edfd59528858b8
%username% <%email%>
2024-05-26 16:50:37 -0400

    Added the method for the Util.java class

--- src/main/java/org/util/Util.java
+++ src/main/java/org/util/Util.java
@@ -1,4 +1,24 @@
 package org.util;
 
 public class Util {
+
+    public static String toTitleCase(String strIn) {
+        if (strIn == null || strIn.isEmpty()) {
+            return strIn;
+        }
+        String[] words = strIn.split(" ");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.length() > 0) {
+                String titleCaseWord = Character.toTitleCase(word.charAt(0)) +
+                        word.substring(1).toLowerCase();
+                result += titleCaseWord;
+                if (i < words.length - 1) {
+                    result += " ";
+                }
+            }
+        }
+        return result;
+    }
 }
